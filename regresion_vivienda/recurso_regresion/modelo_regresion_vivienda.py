81655fd1da192f920a837102204b20890abe7aa4
Marco Canas <marco.canas@udea.edu.co>
2025-03-05 11:19:49 -0500

    empecé el proceso de entrenamiento y selección de modelos

--- regresion_vivienda/recurso_regresion/modelo_regresion_vivienda.py
+++ regresion_vivienda/recurso_regresion/modelo_regresion_vivienda.py
@@ -6,3 +6,5 @@ def save_fig(nombre_archivo_imagen):
     ubicacion_para_imagenes = r"C:/Users/marco/Descargas"
     imagen_path = os.path.join(ubicacion_para_imagenes, nombre_archivo_imagen)
     plt.savefig(imagen_path, bbox_inches='tight')
+    
+    
